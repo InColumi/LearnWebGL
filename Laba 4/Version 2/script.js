e23d93a34b319b4c1a78fb934baa479253d6ff5f
InColumi <max77648@gmail.com>
2022-04-21 15:36:15 +0300

    Aded texture 2, 3, cubetexture

--- Laba 4/Version 2/script.js
+++ Laba 4/Version 2/script.js
@@ -91,66 +91,44 @@ function initShaders(gl, canvas) {
 		tmatloc: gl.getUniformLocation(prog, 'tmat'),
 		projloc: gl.getUniformLocation(prog, 'proj'),
 		tcolloc: gl.getUniformLocation(prog, 'tcol'),
-		textureCoord: gl.getAttribLocation(shaderProgram, 'aTextureCoord'),
+		textureCoord: gl.getAttribLocation(shaderProgram, 'a_texcoord'),
 		uSampler1: gl.getUniformLocation(shaderProgram, 'uSampler1'),
       	uSampler2: gl.getUniformLocation(shaderProgram, 'uSampler2'),
 	};
 
-	console.log(info.textureCoord)
+	texture = {
+		wood:loadTexture('wood.png'),
+		one:loadTexture('1.png'),
+		two:loadTexture('2.png'),
+		three:loadTexture('3.png')
+	};
+	gl.enable(gl.DEPTH_TEST);
 	buffers = initBuffer(gl);
 	space = initSpace();
-	
-	texture = {
-		t1:loadTexture('wood.png'),
-		t2:loadTexture('1.png')
-	};
-
+	console.log(space)
 	update();
 }
 
 function loadTexture(url) {
 	const texture = gl.createTexture();
 	gl.bindTexture(gl.TEXTURE_2D, texture);
-	gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
-	// Because images have to be download over the internet
-	// they might take a moment until they are ready.
-	// Until then put a single pixel in the texture so we can
-	// use it immediately. When the image has finished downloading
-	// we'll update the texture with the contents of the image.
-	const level = 0;
-	const internalFormat = gl.RGBA;
-	const width = 1;
-	const height = 1;
-	const border = 0;
-	const srcFormat = gl.RGBA;
-	const srcType = gl.UNSIGNED_BYTE;
-	const pixel = new Uint8Array([0, 0, 255, 255]);  // opaque blue
-	gl.texImage2D(gl.TEXTURE_2D, level, internalFormat,
-				  width, height, border, srcFormat, srcType,
-				  pixel);
-  
+	gl.texImage2D(
+		gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0,
+		gl.RGBA, gl.UNSIGNED_BYTE, new Uint8Array([0, 0, 255, 255])
+	);
+
 	const image = new Image();
-	image.onload = function() {
-	  gl.bindTexture(gl.TEXTURE_2D, texture);
-	  gl.texImage2D(gl.TEXTURE_2D, level, internalFormat,
-					srcFormat, srcType, image);
-  
-	  // WebGL1 has different requirements for power of 2 images
-	  // vs non power of 2 images so check if the image is a
-	  // power of 2 in both dimensions.
-	  if (isPowerOf2(image.width) && isPowerOf2(image.height)) {
-		 // Yes, it's a power of 2. Generate mips.
-		 gl.generateMipmap(gl.TEXTURE_2D);
-	  } else {
-		 // No, it's not a power of 2. Turn of mips and set
-		 // wrapping to clamp to edge
-		 gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-		 gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-		 gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-	  }
-	};
+	image.crossOrigin = "anonymous";
+	image.onload = function loadTex(){
+		gl.bindTexture(gl.TEXTURE_2D, texture);
+    	gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+      	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+  	};
+
 	image.src = url;
-  
 	return texture;
 }
 
@@ -158,10 +136,6 @@ function loadTexture(url) {
 function update() {
 	drawScene();
 	requestAnimationFrame(update);
-}
-
-function isPowerOf2(value) {
-	return (value & (value - 1)) == 0;
 }
 
 function initBuffer(gl) {
@@ -190,50 +164,27 @@ function initBuffer(gl) {
 		[p110, p101, p100],
 	].flat(2)
 
+	const tx00 = [1,0], tx01 = [0,0];
+	const tx10 = [1, 1], tx11 = [0,1];
+	const tx0 = [tx00, tx10, tx11];
+	const tx1 = [tx00, tx11, tx01];
+	
+	const txy = [
+		tx0, tx1,
+		tx0, tx1,
+		tx0, tx1,
+		tx0, tx1,
+		tx0, tx1,
+		tx0, tx1
+	].flat(2);
 
 	const buffer = gl.createBuffer();
 	gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(pos), gl.STATIC_DRAW);
 
-
 	const textureCoordBuffer = gl.createBuffer();
 	gl.bindBuffer(gl.ARRAY_BUFFER, textureCoordBuffer);
-
-		const textureCoordinates = [
-		// Front
-		0.0,  0.0,
-		1.0,  0.0,
-		1.0,  1.0,
-		0.0,  1.0,
-		// Back
-		0.0,  0.0,
-		1.0,  0.0,
-		1.0,  1.0,
-		0.0,  1.0,
-		// Top
-		0.0,  0.0,
-		1.0,  0.0,
-		1.0,  1.0,
-		0.0,  1.0,
-		// Bottom
-		0.0,  0.0,
-		1.0,  0.0,
-		1.0,  1.0,
-		0.0,  1.0,
-		// Right
-		0.0,  0.0,
-		1.0,  0.0,
-		1.0,  1.0,
-		0.0,  1.0,
-		// Left
-		0.0,  0.0,
-		1.0,  0.0,
-		1.0,  1.0,
-		0.0,  1.0,
-	  ];
-	
-	  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoordinates),
-					gl.STATIC_DRAW);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(txy), gl.STATIC_DRAW);
 
 	return {
 		buf: buffer,
@@ -268,72 +219,72 @@ function initSpace() {
 
 	const _mat = [];
 	const color = [];
+	const t_wood = [];
+	const t_number = [];
 	_mat.push(c1);
 	color.push([1, 0, 0, 1]);
+	t_wood.push(texture.wood);
+	t_number.push(texture.two);
 
 	_mat.push(c2);
 	color.push([0, 1, 0, 1]);
+	t_wood.push(texture.wood);
+	t_number.push(texture.wood);
 
 	_mat.push(c3);
 	color.push([0, 0, 1, 1]);
+	t_wood.push(texture.wood);
+	t_number.push(texture.three);
 
 	_mat.push(c4);
 	color.push([1, 1, 0, 1]);
+	t_wood.push(texture.wood);
+	t_number.push(texture.one);
+
 
 	return {
 		mat: _mat,
 		col: color,
+		t_wood: t_wood,
+		t_number: t_number,
 		size: _mat.length
 	};
 }
 
-function render(tmat, tcol) {
-
+function render(tmat, tcol, t1, t2) {
 	gl.bindBuffer(gl.ARRAY_BUFFER, buffers.buf);
 	gl.vertexAttribPointer(info.vposloc, 3, gl.FLOAT, false, 0, 0);
 	gl.enableVertexAttribArray(info.vposloc);
-
+	
+	gl.bindBuffer(gl.ARRAY_BUFFER, buffers.textureCoord);
+    gl.vertexAttribPointer(info.textureCoord, 2, gl.FLOAT, false, 0, 0);
+	gl.enableVertexAttribArray(info.textureCoord);
+	
 	gl.useProgram(info.prog);
+
 	gl.uniformMatrix4fv(info.tmatloc, false, tmat);
 	gl.uniformMatrix4fv(info.projloc, false, proj);
 	gl.uniform4f(info.tcolloc, false, tcol[0], tcol[1], tcol[2], tcol[3]);
 
-	gl.drawArrays(gl.TRIANGLES, 0, buffers.size);
+	gl.activeTexture(gl.TEXTURE0);
+	gl.bindTexture(gl.TEXTURE_2D, t1);
+	gl.uniform1i(info.uSampler1,  0);
 
-	gl.bindBuffer(gl.ARRAY_BUFFER, buffers.buf);
-	gl.uniform4f(info.tcolloc, false, 0.1, 0.1, 0.1, 1.0);
-	gl.drawArrays(gl.LINES, 0, buffers.size);
+	gl.activeTexture(gl.TEXTURE1);
+	gl.bindTexture(gl.TEXTURE_2D, t2);
+	gl.uniform1i(info.uSampler2,  1);
+
+	gl.drawArrays(gl.TRIANGLES, 0, buffers.size);
 }
 
 function drawScene() {
-	gl.clearColor(1.0, 1.0, 1.0, 1.0);
+	gl.clearColor(0.96, 0.87, 0.7, 1.0);
 	gl.clearDepth(1.0);
 	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-  // Tell WebGL how to pull out the texture coordinates from
-  // the texture coordinate buffer into the textureCoord attribute.
-  {
-    const numComponents = 2;
-    const type = gl.FLOAT;
-    const normalize = false;
-    const stride = 0;
-    const offset = 0;
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffers.textureCoord);
-    gl.vertexAttribPointer(
-        info.textureCoord,
-        numComponents,
-        type,
-        normalize,
-        stride,
-        offset);
-    gl.enableVertexAttribArray(
-        info.textureCoord);
-    console.log(info.textureCoord)
-  }
-
 	for (var i = 0; i < space.size; i++) {
-		// console.log(i)
-		render(space.mat[i], space.col[i]);
+		console.log(space.mat[i])
+		render(space.mat[i], space.col[i], space.t_wood[i], space.t_number[i]);
 	}
 }
 
@@ -342,10 +293,11 @@ function rotateCube(mat, roty) {
 	const x = mat[3];   // mat[3] = 0;
 	const y = mat[7];   // mat[7] = 0;
 	const z = mat[11];  // mat[11] = 0;
-	mat = mat4.rotateY(mat, roty);
+	mat4.rotateY(mat, mat, roty);
 	mat[3] = x;
 	mat[7] = y;
 	mat[11] = z;
+
 }
 
 function rotateCubes(roty) {
@@ -356,7 +308,7 @@ function rotateCubes(roty) {
 	for (var i = 0; i < space.size; i++) {
 		let mat = space.mat[i];
 		mat[3] -= x; mat[7] -= y; mat[11] -= z;
-		mat = mat4.rotateY(mat, roty);
+		mat4.rotateY(mat, mat, roty);
 		mat[3] += x; mat[7] += y; mat[11] += z;
 	}
 }
@@ -364,7 +316,7 @@ function rotateCubes(roty) {
 function rotateStallone(roty) {
 	for (var i = 0; i < space.size; i++) {
 		let mat = space.mat[i];
-		mat = mat4.rotateY(mat, roty);
+		mat4.rotateY(mat, mat, roty);
 	}
 }
 
@@ -373,6 +325,8 @@ window.onkeydown = function kot_blini(event) {
 	let angle = 0.2;
 	if (event.code == "Digit1") {
 		for (let i = 0; i < space.size; i++) {
+			let mat = space.mat[i];
+			console.log(mat)
 			rotateCube(space.mat[i], angle);
 		}
 	}
@@ -395,7 +349,6 @@ function main() {
 	
 	gl = getContext(canvas);
 	gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-	// gl.enable(gl.DEPTH_TEST);
-
+	gl.enable(gl.DEPTH_TEST);
 	initShaders(gl, canvas);
 }
