7ca0b4daf2eb32a1d2b2a3e7d1a99ed0d5704d5a
InColumi <max77648@gmail.com>
2022-05-20 11:20:12 +0300

    Added Laba 7

---
+++ Laba 7/Orange.js
@@ -0,0 +1,133 @@
+/* Здесь всё настраивается для шейдеров (создаются, загружаются и настраиваются связи) */
+function initProgramOrange(gl) {
+  return webglUtils.createProgramInfo(gl, ["vertex-shader-orange", "fragment-shader-orange"]);
+}
+
+/* Загружаем модель из файла и создаём массивы данных и задаём дополнительно цвет */
+/* Взял с интернета и переделал */
+async function initBuffersOrange(gl) {
+const response = await fetch('./model/sphere.obj');  
+const text = await response.text();
+const obj =  parseOBJ(text);
+
+const extents = getGeometriesExtents(obj.geometries);
+orange.yPos = -extents.min[1]
+
+return obj.geometries.map(({data}) => {
+    // Because data is just named arrays like this
+    //
+    // {
+    //   position: [...],
+    //   texcoord: [...],
+    //   normal: [...],
+    // }
+    //
+    // and because those names match the attributes in our vertex
+    // shader we can pass it directly into `createBufferInfoFromArrays`
+    // from the article "less code more fun".
+
+    if (data.color) {
+      if (data.position.length === data.color.length) {
+        // it's 3. The our helper library assumes 4 so we need
+        // to tell it there are only 3.
+        data.color = { numComponents: 3, data: data.color };
+      }
+    } else {
+      // there are no vertex colors so just use constant white
+      data.color = { value: [1, 0.51, 0., 1] };
+    }
+    // create a buffer for each array by calling
+    // gl.createBuffer, gl.bindBuffer, gl.bufferData
+    const bufferInfo = webglUtils.createBufferInfoFromArrays(gl, data);
+    return {
+      bufferInfo,
+    };
+  });
+}
+
+
+function drawOrange(gl, programInfo, buffers) {
+  /* Сбрасываем матрицу */
+  mat4.identity(mvMatrix);
+
+  /* Задаём значения камеры, чтоб апельсин красиво выглядил */
+  mat4.lookAt(mvMatrix, [0, 2, 5], [0, 0, -50], [0,1,0]);
+  /* Задаём координа апельсина */
+  mat4.translate(mvMatrix, mvMatrix, [orange.xPos, orange.yPos, orange.zPos]);
+  /* Вращаем на какой-то угол (управление с кнопок) */
+  mat4.rotate(mvMatrix, mvMatrix, degToRad(orange.yaw), [0, 1, 0]);
+  /* Заполняем матрицу нормали с помощью матрцы модели */
+  mat3.normalFromMat4(nMatrix, mvMatrix);
+
+  gl.useProgram(programInfo.programOrange.program);
+
+  /* Заполняем unifirm - переменные в шейдер */
+  webglUtils.setUniforms(programInfo.programOrange, {
+      u_pMatrix: pMatrix, 
+      u_mvMatrix: mvMatrix,
+      u_nMatrix:nMatrix,
+      u_res:[w, h]
+  });
+
+  /* Заполняем unifirm - переменные в шейдер (свет и текстуру)*/
+  webglUtils.setUniforms(programInfo.programOrange, identicalUniform);
+
+  /* С помощью этого мы рисуем апельсин */
+  for (const {bufferInfo} of buffers.buffersOrange) {
+    // calls gl.bindBuffer, gl.enableVertexAttribArray, gl.vertexAttribPointer
+    webglUtils.setBuffersAndAttributes(gl, programInfo.programOrange, bufferInfo);
+    // calls gl.drawArrays or gl.drawElements
+    webglUtils.drawBufferInfo(gl, bufferInfo);
+  }
+  /* Функция для вращения апельсина */
+  animate()
+}
+
+class Orange {
+  constructor(){
+    /* Общий угол поворота */
+    this.yaw = 0;
+
+    /* на сколько меняем угол при нажатии */
+    this.yawRate = 0;
+
+    this.xPos = 0;
+    this.yPos = 0;
+    this.zPos = 0;
+  }
+}
+
+
+var currentlyPressedKeys = {};
+
+function handleKeyDown(event) {
+  currentlyPressedKeys[event.keyCode] = true;
+}
+
+function handleKeyUp(event) {
+  currentlyPressedKeys[event.keyCode] = false;
+}
+
+function handleKeys() {
+  if (currentlyPressedKeys[37] || currentlyPressedKeys[65]) {
+      // Left cursor key or A
+      orange.yawRate = 0.1;
+  } else if (currentlyPressedKeys[39] || currentlyPressedKeys[68]) {
+      // Right cursor key or D
+      orange.yawRate = -0.1;
+  } else {
+    orange.yawRate = 0;
+  }
+}
+var lastTime = 0;
+var orange = new Orange();
+
+function animate() {
+  var timeNow = new Date().getTime();
+  if (lastTime != 0) {
+    var elapsed = timeNow - lastTime;
+    orange.yaw += orange.yawRate * elapsed;
+
+  }
+  lastTime = timeNow;
+}
---
+++ Laba 7/script.js
@@ -0,0 +1,110 @@
+var identicalUniform;
+let w, h;
+
+window.onload = async function main() {
+  const canvas = document.querySelector('#canvas');
+  const gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
+
+  if (!gl) {
+    alert('Unable to initialize WebGL. Your browser or machine may not support it.');
+    return;
+  }
+
+  /* Запоминаем размер окна и устанавливаем canvas на весь экран */
+  w = canvas.width = window.innerWidth
+  h = canvas.height = window.innerHeight
+
+  /* Событие при изменении размера экрана */
+  window.addEventListener('resize', function() {
+    const canvas = document.querySelector('#canvas');
+    w = canvas.width = window.innerWidth;
+    h = canvas.height = window.innerHeight;
+    gl.viewport(0, 0, w, h);
+  })
+
+  /* Добавляем свои события при нажатии клавиш (чтобы кручить модель) */
+  document.onkeydown = handleKeyDown;
+  document.onkeyup = handleKeyUp;
+
+  const programInfo = {
+    programOrange: initProgramOrange(gl),
+  }
+
+  /* Загружаем текстуру */
+  const collectionTextures = [
+    loadTexture(gl, "texture.png")
+  ]
+
+  /* Создаём буферы */
+  const buffers = {
+    buffersOrange: await initBuffersOrange(gl),
+  }
+
+  function render(now) {
+    /* Функция для проверки нажатия кнопок */
+    handleKeys();
+
+    drawScene(gl, programInfo, buffers, collectionTextures);
+    
+    requestAnimationFrame(render);
+  }
+
+  requestAnimationFrame(render);
+}
+
+/* Функция для загрузки текстуры */
+function loadTexture(gl, url) {
+    var texture = gl.createTexture();
+    var image = new Image();
+    image.src = url;
+    image.addEventListener('load', function() {
+        gl.bindTexture(gl.TEXTURE_2D, texture);
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+        gl.generateMipmap(gl.TEXTURE_2D);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+        gl.bindTexture(gl.TEXTURE_2D, null);
+    });
+    return texture
+}
+
+/* Создаём матрицы для камеры, модели и нормали соответственно */
+var pMatrix = mat4.create();
+var mvMatrix = mat4.create();
+var nMatrix = mat3.create();
+
+function drawScene(gl, programInfo, buffers, collectionTextures) {
+  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+  gl.clearColor(1.0, 1.0, 1.0, 1.0);
+  gl.enable(gl.DEPTH_TEST);
+
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+  /* Задаём матрицу дл камеры */
+  const fieldOfView = degToRad(45);   // in radians
+  const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+  const zNear = 0.1;
+  const zFar = 100.0;
+  mat4.perspective(pMatrix,
+      fieldOfView,
+      aspect,
+      zNear,
+      zFar);
+
+  /* Добавляем в переменную настройки для света и текстуры */
+  identicalUniform = {
+      "lights.ambient": [0.2, 0.2, 0.2],
+      "lights.position": [-2, 2, -1],
+      "lights.diffuse": [0.7, 0.7, 0.7],
+      "lights.specular": [0.9, 0.9, 0.9],
+      "u_texture": collectionTextures[0],
+    }
+  /* Вызываем функцию для отрисовки апельсина */
+  drawOrange(gl, programInfo, buffers);
+}
+
+/* Вспомогательная функция для вычисления градуса */
+function degToRad(degrees) {
+    return degrees * Math.PI / 180;
+}
+
